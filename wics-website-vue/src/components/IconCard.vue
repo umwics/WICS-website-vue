ac340a25e504c72dd5e786190badedd090ed842b
arasantos <100260167+arasantos@users.noreply.github.com>
2024-12-30 20:55:45 -0600

    Create Contact Us Page for tutorial workshop
    Add ContactUsView page and IconCard to show social media information.
    Add social media information in js file.
    Add contact us to router.
    Add color in base.css

---
+++ wics-website-vue/src/components/IconCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="social">
+        <component :is="socialMedia?.svgIcon"></component>
+        <div class="description">
+            <h4>{{ socialMedia?.name }}</h4>
+            <a :href="socialMedia?.link" target="_blank">{{ socialMedia?.username }}</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    socialMedia: { type: Object }
+})
+</script>
+
+<style scoped>
+.social {
+    display: flex;
+}
+
+.description {
+    align-content: center;
+}
+
+svg {
+    width: 100px;
+    height: 100px;
+    fill: var(--color-text-blue);
+}
+
+a {
+    text-decoration: none;
+    color: var(--color-text-dark);
+
+    transition: 0.2s;
+    padding: 2px;
+    padding-left: 10px;
+    padding-right: 10px;
+    cursor: pointer;
+
+    border-radius: 20px;
+}
+
+a:hover {
+    background-color: var(--color-background-dark-hover);
+    color: var(--color-text-light);
+}
+</style>
---
+++ wics-website-vue/src/views/ContactUsView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="view-container">
+        <h1>Contact Us</h1>
+        <p>Have something to share with us? Connect with us on our social media!</p>
+
+        <div class="socials">
+            <div v-for="social in socialMedia" :key="social?.name">
+                <IconCard :socialMedia="social"></IconCard>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import IconCard from '@/components/IconCard.vue';
+import { socialMedia } from '@/assets/data/contactUs/socialMedia.js'
+</script>
+
+<style scoped>
+@media (min-width: 801px) {
+    .socials {
+        display: flex;
+        justify-content: center;
+        gap: 5%;
+    }
+}
+</style>

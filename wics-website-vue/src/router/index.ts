0dae2531f3eba6bd94845eff538b62a6bdbc5989
arasantos <100260167+arasantos@users.noreply.github.com>
2024-12-31 00:18:06 -0600

    Create Contact Us Page
    Add social media js file to contain its information like links.
    Add svg templates for the social media.
    Add contact us to router.
    Create a contact us view with heading and sub heading.

--- wics-website-vue/src/router/index.ts
+++ wics-website-vue/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TeamView from '../views/TeamView.vue'
+import ContactUsView from '@/views/ContactUsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/team',
       name: 'team',
       component: TeamView
+    },
+    {
+      path: '/contact-us',
+      name: 'contactUs',
+      component: ContactUsView
     },
   ]
 })

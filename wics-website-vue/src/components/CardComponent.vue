a319887689bfc41e7caf844f43c3e6a962bfdfdd
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-01-16 07:43:40 -0600

    Create Events page & set image height in CardComponent

--- wics-website-vue/src/components/CardComponent.vue
+++ wics-website-vue/src/components/CardComponent.vue
@@ -48,7 +48,7 @@ export default {
 
 .card-img {
     width: 100%;  
-    height: 100%; 
+    height: 200px; 
     object-fit: cover;
     display: block;
     padding: 0;
---
+++ wics-website-vue/src/views/EventsView.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="view-container">
+        <div class="title">
+            <h1 class="title-head">Events</h1>
+            <p>These are the members of WICS during the 2024-2025 school year.</p>
+        </div>
+        
+        <div class="events-container">
+            <div v-for="event in events" :key="event?.position" class="event">
+                    <CardComponent class="event-card" :image="event.image" :description="event.description" :title="event.title" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { events } from '../assets/data/events/events.js';
+import CardComponent from '../components/CardComponent.vue'
+</script>
+
+<style scoped>
+    .title {
+        padding-bottom: 4%;
+    }
+
+    .title-head {
+        padding-bottom: 1%;
+    }
+
+    @media (max-width: 800px) {
+        .events-container{
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-gap: 2rem;
+        }
+    } 
+
+    @media (min-width: 801px) {
+        .events-container{
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-column-gap: 2rem;
+            grid-row-gap: 1.5rem;    
+        }
+
+        .event{
+            padding: 2%;
+            align-self: stretch;
+            margin: 0;
+            display: flex;
+        }
+    }
+</style>

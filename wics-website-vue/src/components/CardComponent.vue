e6d0f9ed39d74acfbbbe0b00b45942aa4281bb49
arasantos <100260167+arasantos@users.noreply.github.com>
2025-01-21 13:48:23 -0600

    Add option for image position in card component
    Add option for image position in card component, only show image to the side if screen is big enough. Showing the image to the side when the screen is small looks off.
    Create useValidator composable to be used for props that have specific values. Can add more validation criteria in the future as needed.
    Change CardComponent to use script setup to be consistent with other files, and use h2 to achieve the same size without modifying h1.
    Document the use of BEM naming convention (prop-name--prop-value) for class names of props with specific values.

--- wics-website-vue/src/components/CardComponent.vue
+++ wics-website-vue/src/components/CardComponent.vue
@@ -2,22 +2,43 @@
     <div class="card-container">
         <div class="card-top"></div>
         <div class="main">
-            <img class="card-img" :src="image" />
-            <h1 class="title">{{ title }}</h1>
-            <p class="body">{{ description }}</p>
+            <div :class="imagePositionClasses">
+                <div class="card-img">
+                    <img :v-if="image" :src="image" />
+                </div>
+                <div class="card-text">
+                    <h2 class="title">{{ title }}</h2>
+                    <p class="body">{{ description }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'CardComponent',
-    props: {
-        image: String,
-        title: String,
-        description: String,
-    }
-}
+<script setup>
+import { useValidator } from '@/composables/useValidator.js'
+
+const name = 'CardComponent';
+
+defineOptions({
+    name: name,
+});
+
+const props = defineProps({
+    imagePosition: {
+        type: String,
+        default: 'top'
+    },
+    image: { type: String },
+    title: { type: String },
+    description: { type: String },
+});
+
+// validates imagePosition prop values as it has specific valid options 
+const validValues = ['top', 'side'];
+useValidator(props.imagePosition, name, validValues);
+
+const imagePositionClasses = 'image-position--' + props.imagePosition;
 </script>
 
 <style scoped>
@@ -33,18 +54,39 @@ export default {
 }
 
 .card-img {
-    width: 100%;
-    height: 100%;
     object-fit: cover;
     display: block;
     padding: 0;
     margin: 0;
+}
+
+.image-position--top .card-img {
     margin-bottom: 30px;
+    width: 100%;
+    height: 100%;
+}
+
+/* the image only goes to the side for bigger screens */
+@media (min-width: 801px) {
+    .image-position--side {
+        display: flex;
+        flex-direction: row;
+        column-gap: 4%;
+        align-items: center;
+    }
+
+    .image-position--side .card-text {
+        width: 100%;
+    }
+
+    .image-position--side .card-img {
+        width: 60%;
+        height: 100%;
+    }
 }
 
 .title {
     font-weight: bold;
-    font-size: 24px;
     padding: 0;
     margin: 0;
     margin-bottom: 20px;
@@ -54,5 +96,9 @@ export default {
     font-size: 16px;
     padding: 0;
     margin: 0;
+
+    white-space: pre-line
 }
+
+/* maybe just use grid template? */
 </style>

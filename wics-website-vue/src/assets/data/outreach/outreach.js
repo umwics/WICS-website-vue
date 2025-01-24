f69b6d1107ffc614f7ce483f734c1e4122821a39
Yashsavi Negi <negiyashsavi@gmail.com>
2025-01-24 00:28:46 -0600

    Outreach information file

---
+++ wics-website-vue/src/assets/data/outreach/outreach.js
@@ -0,0 +1,22 @@
+import fortRichmondCollegiate from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\Fort Richmond Logo_Vertical_Motto Wide.jpg';
+import gamedevworkshop from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\game dev workshop.png';
+import discord from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\discord.png';
+
+
+export const outreach = [
+    {
+        "image": fortRichmondCollegiate,
+        "title": "School Presentation",
+        "description": "Some of our WICS members visited the Fort Richmond Collegiate and gave a presentation about WICS! Afterwards, we had a panel where the students were able to ask questions they wanted, including questions about WICS community events, Computer Science, university tips, and many more!",
+    },
+    {
+        "image": gamedevworkshop,
+        "title": "Game Development Workshops",
+        "description": "Last fall, WICS Outreach hosted game development workshops for grade 5-8 students! We taught them how to create their own 2D-platform game using Gamefroot. This is all in partnership with CanU, a Winnipeg-based charitable organization.",
+    },
+    {
+        "image": discord,
+        "title": "Outreach Discord Server",
+        "description": "Interested in volunteering with us? Join our server below to get more information about our volunteer opportunities! Sign up here (https://discord.gg/G98TGzG3UD)",
+    }
+];

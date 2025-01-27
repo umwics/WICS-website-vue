f600b3f96acaedf10fd360c8b5ca447501e445ec
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-01-27 02:40:26 -0600

    Create CollageComponent.vue

---
+++ wics-website-vue/src/components/CollageComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="collage-container">
+        <img v-for="item in content.images" :key="item?.position" :src="item.imageSrc" :alt="item.alt" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CollageComponent',
+    props: {
+        content: Object
+    }
+}
+</script>
+
+<style scoped>
+.collage-container {
+    max-width: 800px;
+    margin-inline: auto;
+    display: grid;
+    gap: 0.5rem;
+    grid-template-areas: 
+    "one   one   five five five two  two"
+    "three three five five five four four";
+}
+
+.collage-container img {
+    width: 100%;
+    aspect-ratio: 1;
+    display: block;
+    object-fit: cover;
+    align-self: center;
+}
+
+.collage-container :nth-child(1) {
+    grid-area: one;
+}
+
+.collage-container :nth-child(2) {
+    grid-area: two;
+}
+
+.collage-container :nth-child(3) {
+    grid-area: three;
+}
+
+.collage-container :nth-child(4) {
+    grid-area: four;
+}
+
+.collage-container :nth-child(5) {
+    grid-area: five;
+} 
+</style>

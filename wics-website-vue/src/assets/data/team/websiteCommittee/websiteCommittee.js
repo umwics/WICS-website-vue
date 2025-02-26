832fe713c87a9b33c2af674c71dbf998b1432da9
Yashsavi Negi <negiyashsavi@gmail.com>
2025-02-25 20:37:26 -0600

    team information changes

--- wics-website-vue/src/assets/data/team/websiteCommittee/websiteCommittee.js
+++ wics-website-vue/src/assets/data/team/websiteCommittee/websiteCommittee.js
@@ -1,13 +1,12 @@
 import websiteCoordinatorImage from './images/Ara.jpg';
-import websiteMember1 from '../defaultMember.jpeg';
-import websiteMember2 from './images/KC.webp';
-import websiteMember3 from '../defaultMember.jpeg';
+import websiteMember1 from './images/KC.webp';
+import websiteMember2 from './images/Yashsavi.jpg';
+import websiteMember3 from './images/corrina_img.webp';
 import websiteMember4 from '../defaultMember.jpeg';
-import websiteMember5 from './images/Yashsavi.jpg';
+import websiteMember5 from '../defaultMember.jpeg';
 import websiteMember6 from '../defaultMember.jpeg';
 import websiteMember7 from '../defaultMember.jpeg';
 import websiteMember8 from '../defaultMember.jpeg';
-import websiteMember9 from './images/corrina_img.webp';
 
 export const websiteCommittee = [
     {
@@ -21,42 +20,15 @@ export const websiteCommittee = [
     },
     {
         "image": websiteMember1,
-        "name": "Mandeep Kaur",
-        "position": "Website Committee Member",
-        "memberType": "Contributor",
-        "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Mandeep Kaur, WICS Website Committee Member"
-    },
-    {
-        "image": websiteMember2,
         "name": "Kriza Cyrene (KC) del Moro",
         "position": "Website Committee Member",
         "memberType": "Contributor",
         "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description": "Hey everyone I'm KC, a second-year computer science student. I love getting to know new people, listening to music, and doing illustrations! Art is something I wanted to keep as a hobby, and I enjoyed Math from a young age so I joined Computer Science. I love that it's a career path all about learning and problem solving. I joined WICS because I wanted to become a part of a team that empowers voices of women and gender minorities, especially in tech!",
         "altImage": "Image of Kriza Cyrene (KC) del Moro, WICS Website Committee Member"
     },
     {
-        "image": websiteMember3,
-        "name": "Krisha Bhalala",
-        "position": "Website Committee Member",
-        "memberType": "Contributor",
-        "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Krisha Bhalala, WICS Website Committee Member"
-    },
-    {
-        "image": websiteMember4,
-        "name": "Nataniella Ogogo",
-        "position": "Website Committee Member",
-        "memberType": "Contributor",
-        "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Nataniella Ogogo, WICS Website Committee Member"
-    },
-    {
-        "image": websiteMember5,
+        "image": websiteMember2,
         "name": "Yashsavi Negi",
         "position": "Website Committee Member",
         "memberType": "Contributor",
@@ -65,25 +37,34 @@ export const websiteCommittee = [
         "altImage": "Image of Yashsavi Negi, WICS Website Committee Member"
     },
     {
+        "image": websiteMember3,
+        "name": "Corrina Colvinson",
+        "position": "Website Committee Member",
+        "memberType": "Contributor",
+        "pronouns": " ",
+        "description": "Hello! My name is Corrina and I'm a first year computer science student! I love talking with people, teaching kids theatre, playing boardgames, and starting new book series! In terms of CS, I'm passionate about problem-solving and how innovations in computer science can be used as tools for education. I'm also very excited about working towards a future where everyone feels empowered to thrive in their passions!",
+        "altImage": "Image of Corrina Colvinson, WICS Website Committee Member"
+    },
+    {
+        "image": websiteMember4,
+        "name": "Mandeep Kaur",
+        "position": "Website Committee Member",
+        "memberType": "Contributor",
+        "pronouns": " ",
+        "description": "Hi everyone! My name in Mandeep, but you can call me Mandy. I’m currently in my 1st year of university. I am passionate about web development and problem-solving, and I’m excited to contribute to the website committee. My hobbies include reading books and running.",
+        "altImage": "Image of Mandeep Kaur, WICS Website Committee Member"
+    },
+    {
+        "image": websiteMember5,
+        "name": "Krisha Bhalala",
+        "position": "Website Committee Member",
+        "memberType": "Contributor",
+        "pronouns": " ",
+        "description": "Hi, I'm Krisha, a third-year Computer Science student. I enjoy exploring city, finding cute cafes, and discovering cool new spots. As a WICS committee member, I'm super passionate about supporting women in tech and creating spaces where everyone feels welcome. I'm excited to push myself, try new things, and show that tech is for everyone – especially girls who want to make a difference!",
+        "altImage": "Image of Krisha Bhalala, WICS Website Committee Member"
+    },
+    {
         "image": websiteMember6,
-        "name": "Esther Akamnonu",
-        "position": "Website Committee Member",
-        "memberType": "Contributor",
-        "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Esther Akamnonu, WICS Website Committee Member"
-    },
-    {
-        "image": websiteMember7,
-        "name": "Marda",
-        "position": "Website Committee Member",
-        "memberType": "Contributor",
-        "pronouns": " ",
-        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Marda, WICS Website Committee Member"
-    },
-    {
-        "image": websiteMember8,
         "name": "Jillian Manguba",
         "position": "Website Committee Member",
         "memberType": "Contributor",
@@ -92,12 +73,21 @@ export const websiteCommittee = [
         "altImage": "Image of Jillian Manguba, WICS Website Committee Member"
     },
     {
-        "image": websiteMember9,
-        "name": "Corrina Colvinson",
+        "image": websiteMember7,
+        "name": "Warda Zulfiqar",
         "position": "Website Committee Member",
         "memberType": "Contributor",
         "pronouns": " ",
         "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "altImage": "Image of Corrina Colvinson, WICS Website Committee Member"
+        "altImage": "Image of Marda, WICS Website Committee Member"
+    },
+    {
+        "image": websiteMember8,
+        "name": "Nataniella Ogogo",
+        "position": "Website Committee Member",
+        "memberType": "Contributor",
+        "pronouns": " ",
+        "description": "This is a default message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "altImage": "Image of Nataniella Ogogo, WICS Website Committee Member"
     }
 ];

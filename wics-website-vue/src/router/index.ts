b20ece00a8776a346a9aca39b66894dd121bdc95
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-02-27 17:34:10 -0600

    create-an-outreach-page

--- wics-website-vue/src/router/index.ts
+++ wics-website-vue/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TeamView from '../views/TeamView.vue'
+import OutreachView from '../views/OutreachView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'team',
       component: TeamView
     },
+    {
+      path: '/outreach',
+      name: 'outreach',
+      component: OutreachView
+    }
   ]
 })
 

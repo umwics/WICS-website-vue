d2f1fda65813dfb4653c65dea6598ca256d1dd03
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-02-27 16:53:52 -0600

    updated imports

--- wics-website-vue/src/assets/data/outreach/outreach.js
+++ wics-website-vue/src/assets/data/outreach/outreach.js
@@ -1,7 +1,6 @@
-import fortRichmondCollegiate from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\images\Fort Richmond Logo_Vertical_Motto Wide.jpg';
-import gamedevworkshop from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\images\game dev workshop.png';
-import discord from 'C:\Users\robot\OneDrive - University of Manitoba\Desktop\wicsWebsite\WICS-website-vue\wics-website-vue\src\assets\data\outreach\images\discord.png';
-
+import fortRichmondCollegiate from './images/fort-richmond-collegiate.jpg';
+import gameDevWorkshop from './images/game-dev-workshop.png';
+import outreachDiscordServer from './images/outreach-discord-server.png';
 
 export const outreach = [
     {
@@ -10,12 +9,12 @@ export const outreach = [
         "description": "Some of our WICS members visited the Fort Richmond Collegiate and gave a presentation about WICS! Afterwards, we had a panel where the students were able to ask questions they wanted, including questions about WICS community events, Computer Science, university tips, and many more!",
     },
     {
-        "image": gamedevworkshop,
+        "image": gameDevWorkshop,
         "title": "Game Development Workshops",
         "description": "Last fall, WICS Outreach hosted game development workshops for grade 5-8 students! We taught them how to create their own 2D-platform game using Gamefroot. This is all in partnership with CanU, a Winnipeg-based charitable organization.",
     },
     {
-        "image": discord,
+        "image": outreachDiscordServer,
         "title": "Outreach Discord Server",
         "description": "Interested in volunteering with us? Join our server below to get more information about our volunteer opportunities! Sign up here (https://discord.gg/G98TGzG3UD)",
     }

2985fd05d3813465b629ccb612d9e1e5241facc5
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-02-27 17:34:10 -0600

    create-an-outreach-page

---
+++ wics-website-vue/src/views/OutreachView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="view-container">
+        <div class="title">
+            <h1>UMWICS Outreach</h1>
+            <p>These are the outreach events and workshops WICS hoted this year!</p>
+        </div>
+        <div class="outreach-container">
+            <div v-for="item in outreach" :key="outreach?.position" class="item">
+                <CardComponent class="outreach-card" :image="item.image" :title="item.title" :description="item.description"  />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { outreach } from '../assets/data/outreach/outreach.js' 
+import CardComponent from '../components/CardComponent.vue'
+</script>
+
+<style scoped>
+.title {
+    padding-bottom: 3%;
+}
+@media (max-width: 800px) {
+    .outreach-container{
+
+    }
+}
+
+@media (min-width: 801px) {
+    .outreach-container{
+    }
+}
+</style>

8e78a316da9a1caea3397e76dfc2bee10284d81f
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-03-02 19:27:00 -0600

    Update OutreachView.vue and outreach.js

--- wics-website-vue/src/assets/data/outreach/outreach.js
+++ wics-website-vue/src/assets/data/outreach/outreach.js
@@ -6,16 +6,16 @@ export const outreach = [
     {
         "image": fortRichmondCollegiate,
         "title": "School Presentation",
-        "description": "Some of our WICS members visited the Fort Richmond Collegiate and gave a presentation about WICS! Afterwards, we had a panel where the students were able to ask questions they wanted, including questions about WICS community events, Computer Science, university tips, and many more!",
+        "description": "Some of our WICS members visited the Fort Richmond Collegiate and gave a presentation about WICS!\n\nAfterwards, we had a panel where the students were able to ask questions they wanted, including questions about WICS community events, Computer Science, university tips, and many more!",
     },
     {
         "image": gameDevWorkshop,
         "title": "Game Development Workshops",
-        "description": "Last fall, WICS Outreach hosted game development workshops for grade 5-8 students! We taught them how to create their own 2D-platform game using Gamefroot. This is all in partnership with CanU, a Winnipeg-based charitable organization.",
+        "description": "Last fall, WICS Outreach hosted game development workshops for grade 5-8 students! We taught them how to create their own 2D-platform game using Gamefroot.\n\nThis is all in partnership with CanU, a Winnipeg-based charitable organization.",
     },
     {
         "image": outreachDiscordServer,
         "title": "Outreach Discord Server",
-        "description": "Interested in volunteering with us? Join our server below to get more information about our volunteer opportunities! Sign up here (https://discord.gg/G98TGzG3UD)",
+        "description": "Interested in volunteering with us?\n\nJoin our server below to get more information about our volunteer opportunities! Sign up here (https://discord.gg/G98TGzG3UD)",
     }
 ];
--- wics-website-vue/src/views/OutreachView.vue
+++ wics-website-vue/src/views/OutreachView.vue
@@ -5,8 +5,8 @@
             <p>These are the outreach events and workshops WICS hoted this year!</p>
         </div>
         <div class="outreach-container">
-            <div v-for="item in outreach" :key="item?.position" class="outreach">
-                <CardComponent class="outreach-card" :image="item.image" :title="item.title" :description="item.description"  />
+            <div v-for="item in outreach" :key="item?.position" class="outreach-card">
+                <CardComponent :imagePosition="position" :image="item.image" :title="item.title" :description="item.description"  />
             </div>
         </div>
     </div>
@@ -15,20 +15,32 @@
 <script setup>
 import { outreach } from '../assets/data/outreach/outreach.js' 
 import CardComponent from '../components/CardComponent.vue'
+const position = 'side'
 </script>
 
 <style scoped>
 .title {
     padding-bottom: 3%;
 }
-@media (max-width: 800px) {
-    .outreach-container{
 
+@media (max-width: 800px) {
+    .outreach-card {
+        padding-bottom: 5%;
     }
 }
 
 @media (min-width: 801px) {
-    .outreach-container{
+    .outreach-card {
+        padding-bottom: 3%;
     }
 }
+
+.outreach-card:last-of-type {
+    padding-bottom: 0;
+}
+
+.outreach-card {
+    white-space: pre-line;
+}
+
 </style>

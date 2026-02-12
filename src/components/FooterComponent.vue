49d50beb657186f41590728aec2291a0238fce6e
Halah <halahmhanni@MacBook-Pro-120.local>
2026-02-11 21:37:22 -0600

    Ran formatter

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,9 @@
 <template>
   <footer>
     <div class="footer-content">
-      <p>© {{currentYear}} University of Manitoba Women in Computer Science. All Rights Reserved.</p>
+      <p>
+        © {{ currentYear }} University of Manitoba Women in Computer Science. All Rights Reserved.
+      </p>
       <p>
         This work is licensed under a Creative Commons Attribute-NonCommercial-ShareAlike 4.0
         International License.
@@ -11,7 +13,7 @@
 </template>
 
 <script setup>
-const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear()
 </script>
 
 <style scoped>

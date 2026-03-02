27dbd2e67205be77059d08220e93d1302098c95f
Halah <halahmhanni@MacBook-Pro-120.local>
2026-03-02 17:43:26 -0600

    Add assistant names to member list

--- src/assets/data/team/events/events.js
+++ src/assets/data/team/events/events.js
@@ -1,5 +1,5 @@
 import eventsCoordinatorImage from './images/Rimsha.jpg'
-
+import asstEventsCoordinatorImage from '.../team/defaultMember.jpeg'
 export const events = [
   {
     image: eventsCoordinatorImage,
@@ -10,14 +10,14 @@ export const events = [
     description: "Hi everyone >_<, my name is Rimsha and I am this years Events Coordinator! I am currently in my second year doing a cs honours degree with a math minor. You might know me from WICS last year when I was the assistant events coordinator or you might know me as your TA. Some fun facts about me is that I am a graphic designer, I love to bake and I like to collect figurines :) I am so excited to take over the role of Events Coordinator this year and I hope to bring exciting technical and social events to our community and offer a safe space for women and gender minorities that are having a hard time finding likeminded people or a space to showcase their interests!",
     altImage: 'Image of Rimsha Irfan, WICS Events Coordinator'
   },
-  // {
-  //   image: asstEventsCoordinatorImage,
-  //   name: 'Rimsha Irfan',
-  //   position: 'Assistant Events Coordinator',
-  //   memberType: 'Appointed',
-  //   pronouns: 'she/her',
-  //   description:
-  //     'Hi! My name is Rimsha, I am the Assistant Events Coordinator for this year! I am currently in my second year of Computer Science Major with a Minor in math. I am very grateful to be part of the WICS team this year and work along with the others in my team to bring creative and fun events to you guys. I can’t wait for you guys to attend and experience our upcoming events and opportunities. Other than being part of WICS some other things I enjoy are weightlifting, graphic design and baking',
-  //   altImage: 'Image of Rimsha Irfan, WICS Assistant Events Coordinator'
-  // }
+  {
+    image: asstEventsCoordinatorImage,
+    name: 'Edrielle',
+    position: 'Assistant Events Coordinator',
+    memberType: 'Appointed',
+    pronouns: 'she/her',
+    description: ""
+    ,
+        altImage: 'Image of Rimsha Irfan, WICS Assistant Events Coordinator'
+  }
 ]
--- src/assets/data/team/outreach/outreach.js
+++ src/assets/data/team/outreach/outreach.js
@@ -11,14 +11,13 @@ export const outreach = [
       "Hi! My name is Dalia Bashaev and I'm your Outreach Coordinator for this year. I'm a third year computer science student! I'm excited to be back on the team this year after being on the team last year. I'm looking forward to continuing the workshops from last year and expanding what WICs had accomplished by getting to talk about WICS with more high school students! A little bit about me: I like rock climbing, reading books, and baking!",
     altImage: 'Image of Dalia Bashaev, WICS Outreach Coordinator'
   },
-  // {
-  //   image: asstOutreachCoordinatorImage,
-  //   name: 'Jasmine Shapan',
-  //   position: 'Assistant Outreach Coordinator',
-  //   memberType: 'Appointed',
-  //   pronouns: 'she/her',
-  //   description:
-  //     'Hi! My name is Jasmine and i’m the Assistant Outreach Coordinator for this year! I’m currently in my 3rd year majoring in Computer Science and minoring in Statistics. My role involves me helping out the Outreach Coordinator by running workshops, creating presentations, and overall helping out our community! This is my first time being on the WICS team and it has been the best! Everyone is so kind and helpful so if you ever see us, be sure to say hi! Outside of school, I enjoy many hobbies! Playing my guitar and piano, running, and Netflix (Currently watching Arcane, so good!).',
-  //   altImage: 'Image of Jasmine Shapan, WICS Assistant Outreach Coordinator'
-  // }
+  {
+    image: asstOutreachCoordinatorImage,
+    name: 'Corrina',
+    position: 'Assistant Outreach Coordinator',
+    memberType: 'Appointed',
+    pronouns: 'she/her',
+    description: '',
+        altImage: 'Image of Jasmine Shapan, WICS Assistant Outreach Coordinator'
+  }
 ]

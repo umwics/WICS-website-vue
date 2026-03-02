327bdcc7a3e3c02bbb909d079c49818ba94a6934
Halah <halahmhanni@MacBook-Pro-120.local>
2026-03-02 16:01:57 -0600

    Updated social media coordinator

--- src/assets/data/team/outreach/outreach.js
+++ src/assets/data/team/outreach/outreach.js
@@ -9,17 +9,17 @@ export const outreach = [
     memberType: 'Executive',
     pronouns: 'she/her',
     description:
-      "Hi! My name is Dalia and I am the Outreach Coordinator! I am a second-year student majoring in Computer Science and minoring in Math. I am so excited to be a part of WICS this year and work with this great team! I can’t wait to see how our outreach opportunities go this year and I'm looking forward to working with great people! A fun fact about me is that I love playing cozy games. Currently, I have been playing Animal Crossing and Stardew Valley, and I’m hoping to try out MySims soon!",
+      "Hi! My name is Dalia Bashaev and I'm your Outreach Coordinator for this year. I'm a third year computer science student! I'm excited to be back on the team this year after being on the team last year. I'm looking forward to continuing the workshops from last year and expanding what WICs had accomplished by getting to talk about WICS with more high school students! A little bit about me: I like rock climbing, reading books, and baking!",
     altImage: 'Image of Dalia Bashaev, WICS Outreach Coordinator'
   },
-  {
-    image: asstOutreachCoordinatorImage,
-    name: 'Jasmine Shapan',
-    position: 'Assistant Outreach Coordinator',
-    memberType: 'Appointed',
-    pronouns: 'she/her',
-    description:
-      'Hi! My name is Jasmine and i’m the Assistant Outreach Coordinator for this year! I’m currently in my 3rd year majoring in Computer Science and minoring in Statistics. My role involves me helping out the Outreach Coordinator by running workshops, creating presentations, and overall helping out our community! This is my first time being on the WICS team and it has been the best! Everyone is so kind and helpful so if you ever see us, be sure to say hi! Outside of school, I enjoy many hobbies! Playing my guitar and piano, running, and Netflix (Currently watching Arcane, so good!).',
-    altImage: 'Image of Jasmine Shapan, WICS Assistant Outreach Coordinator'
-  }
+  // {
+  //   image: asstOutreachCoordinatorImage,
+  //   name: 'Jasmine Shapan',
+  //   position: 'Assistant Outreach Coordinator',
+  //   memberType: 'Appointed',
+  //   pronouns: 'she/her',
+  //   description:
+  //     'Hi! My name is Jasmine and i’m the Assistant Outreach Coordinator for this year! I’m currently in my 3rd year majoring in Computer Science and minoring in Statistics. My role involves me helping out the Outreach Coordinator by running workshops, creating presentations, and overall helping out our community! This is my first time being on the WICS team and it has been the best! Everyone is so kind and helpful so if you ever see us, be sure to say hi! Outside of school, I enjoy many hobbies! Playing my guitar and piano, running, and Netflix (Currently watching Arcane, so good!).',
+  //   altImage: 'Image of Jasmine Shapan, WICS Assistant Outreach Coordinator'
+  // }
 ]

322fa863e47ad137e8d72dbc6d58e0db20b94a89
Halah <halahmhanni@MacBook-Pro-120.local>
2026-03-02 16:42:54 -0600

    Updated website committee members

--- src/assets/data/team/websiteCommittee/websiteCommittee.js
+++ src/assets/data/team/websiteCommittee/websiteCommittee.js
@@ -1,99 +1,51 @@
 import websiteCoordinatorImage from './images/Ara.jpg'
-import websiteMember1 from './images/KC.webp'
-import websiteMember2 from './images/Yashsavi.jpg'
-import websiteMember3 from './images/corrina_img.webp'
-import websiteMember4 from '../defaultMember.jpeg'
-import websiteMember5 from '../defaultMember.jpeg'
-import websiteMember6 from '../defaultMember.jpeg'
-import websiteMember7 from '../defaultMember.jpeg'
-import websiteMember8 from '../defaultMember.jpeg'
+import defaultMember from '../defaultMember.jpeg'
+
 
 export const websiteCommittee = [
   {
     image: websiteCoordinatorImage,
-    name: 'Ara Nicole Santos',
+    name: 'Halah Mhanni',
     position: 'Website Committee Coordinator',
     memberType: 'Appointed',
     pronouns: 'she/her',
-    description:
-      "Hi everyone! I'm Ara and I'm the Website Committee Coordinator for 2024-2025! I'm a 4th year CS major, and I'm also in co-op! I'm very grateful to be a part of WICS as it's such a welcoming community, full of hard working and supportive people. I'm so excited to be leading the creation of our new website! Fun fact about me is I love to collect earrings. My most favorites are my boba, cloud, and duck earrings! If you see me out there, feel free to say hi. I love meeting new people :D",
-    altImage: 'Image of Ara Nicole Santos, WICS Website Committee Coordinator'
+    description: "Hi, I’m Halah!! :D I’m the Website Coordinator for WICS this year. I’m in my fourth year of a CS Honours degree. I started out with WICS in my first year as a volunteer, and it’s been such an amazing experience being involved with WICS every year. I highly encourage everyone to get involved- it’s such a welcoming and supportive community, and I’ve met so many amazing people through WICS^.^ <3",
+    altImage: 'Image of Halah Mhanni, WICS Website Committee Coordinator'
   },
   {
-    image: websiteMember1,
-    name: 'Kriza Cyrene (KC) del Moro',
-    position: 'Website Committee Member',
-    memberType: 'Contributor',
-    pronouns: 'they/them',
-    description:
-      "Hey everyone I'm KC, a second-year computer science student. I love getting to know new people, listening to music, and doing illustrations! Art is something I wanted to keep as a hobby, and I enjoyed Math from a young age so I joined Computer Science. I love that it's a career path all about learning and problem solving. I joined WICS because I wanted to become a part of a team that empowers voices of women and gender minorities, especially in tech!",
-    altImage: 'Image of Kriza Cyrene (KC) del Moro, WICS Website Committee Member'
-  },
-  {
-    image: websiteMember2,
-    name: 'Yashsavi Negi',
+    image: defaultMember,
+    name: 'Jack',
     position: 'Website Committee Member',
     memberType: 'Contributor',
     pronouns: 'she/her',
-    description:
-      'Hi, I am Yashsavi Negi aka Yashi. I am a Computer Science Honour student minoring in Business for Leadership and Organization. I love working on this website and learning from others. I love dancing, reading and swimming. If you ever see me, feel free to say hi. I love talking or we can just yap :)',
-    altImage: 'Image of Yashsavi Negi, WICS Website Committee Member'
+    description:"hi, i'm jack! i'm starting out in my first year of computer science, and i'm so excited to be able to contribute to empowering women and gender minorities through WICS. i was raised in the java virtual machine, and first got into CS through making minecraft mods. outside of school, i enjoy reading, running, and writing!",
+    altImage: ''
   },
   {
-    image: websiteMember3,
-    name: 'Corrina Colvinson',
+    image: defaultMember,
+    name: 'Mercy',
     position: 'Website Committee Member',
     memberType: 'Contributor',
     pronouns: 'she/her',
-    description:
-      "Hello! My name is Corrina and I'm a first year computer science student! I love talking with people, teaching kids theatre, playing boardgames, and starting new book series! In terms of CS, I'm passionate about problem-solving and how innovations in computer science can be used as tools for education. I'm also very excited about working towards a future where everyone feels empowered to thrive in their passions!",
-    altImage: 'Image of Corrina Colvinson, WICS Website Committee Member'
+    description: "Hey everyone! My name is Mercy, and I’m a second-year Computer Science student. I’m excited to be part of the WICS website committee because I’m interested in learning more web development skills and eager to help support gender minorities in CS. In my spare time, I enjoy crocheting, reading, and watching true crime shows.",
+    altImage: ''
   },
   {
-    image: websiteMember4,
-    name: 'Mandeep Kaur',
-    position: 'Website Committee Member',
-    memberType: 'Contributor',
-    pronouns: '',
-    description:
-      'Hi everyone! My name in Mandeep, but you can call me Mandy. I’m currently in my 1st year of university. I am passionate about web development and problem-solving, and I’m excited to contribute to the website committee. My hobbies include reading books and running.',
-    altImage: 'Image of Mandeep Kaur, WICS Website Committee Member'
-  },
-  {
-    image: websiteMember5,
-    name: 'Krisha Bhalala',
-    position: 'Website Committee Member',
-    memberType: 'Contributor',
-    pronouns: '',
-    description:
-      "Hi, I'm Krisha, a third-year Computer Science student. I enjoy exploring city, finding cute cafes, and discovering cool new spots. As a WICS committee member, I'm super passionate about supporting women in tech and creating spaces where everyone feels welcome. I'm excited to push myself, try new things, and show that tech is for everyone – especially girls who want to make a difference!",
-    altImage: 'Image of Krisha Bhalala, WICS Website Committee Member'
-  },
-  {
-    image: websiteMember6,
-    name: 'Jillian Manguba',
+    image: defaultMember,
+    name: 'Simi',
     position: 'Website Committee Member',
     memberType: 'Contributor',
     pronouns: 'she/her',
-    description: '',
-    altImage: 'Image of Jillian Manguba, WICS Website Committee Member'
+    description: "Hiii!! My name is Simi, and I’m in my third year as a Comp Science student. I joined WICS website committee to learn more about web development and I’m eager to contribute as much as I can, where I can. In my spare time, I enjoy sleeping and watching cop/investigative shows.",
+    altImage: ''
   },
   {
-    image: websiteMember7,
-    name: 'Warda Zulfiqar',
-    position: 'Website Committee Member',
-    memberType: 'Contributor',
-    pronouns: '',
-    description: '',
-    altImage: 'Image of Marda, WICS Website Committee Member'
-  },
-  {
-    image: websiteMember8,
-    name: 'Nataniella Ogogo',
+    image: defaultMember,
+    name: 'Lucy',
     position: 'Website Committee Member',
     memberType: 'Contributor',
     pronouns: 'she/her',
-    description: '',
-    altImage: 'Image of Nataniella Ogogo, WICS Website Committee Member'
-  }
+    description:"Intro: Hey! I’m Xinyue, you can just call me Lucy! I’m a third-year Computer Science honors student. I enjoy working with code behind the scenes, also I’m expecting to meet more friends in wics! Outside of class I am particularly interested in video games and playing guitar.",   
+    altImage: '',
+  },
 ]

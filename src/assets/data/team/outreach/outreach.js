9afd83ae2bd0582552866ee23a9285aee51e586e
Halah <halahmhanni@MacBook-Pro-120.local>
2026-03-03 14:33:09 -0600

    Add Corrina's photo and description

--- src/assets/data/team/outreach/outreach.js
+++ src/assets/data/team/outreach/outreach.js
@@ -1,5 +1,5 @@
 import outreachCoordinatorImage from './images/Dalia.JPG'
-import asstOutreachCoordinatorImage from '../defaultMember.jpeg'
+import asstOutreachCoordinatorImage from './images/Corrina.png'
 
 export const outreach = [
   {
@@ -18,6 +18,6 @@ export const outreach = [
     position: 'Assistant Outreach Coordinator',
     memberType: 'Appointed',
     pronouns: 'she/her',
-    description: '',
+    description: 'Hello! My name is Corrina and I’m a second year student in the computer science honours program. I’m passionate about outreach and helping children and youth see the fun in computer science and develop creative thinking and problem solving skills! Outside of my school and work, I’m an avid reader and love meeting and chatting with new people! ',
   }
 ]
